ecaf273e91a28ebf2dcd6ccaa75102c299c4d875
Aleksandr Sokolovskii <amsokol@gmail.com>
2017-06-03 22:42:15 +0300

    added server rendering

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent
   ],
   imports: [
-    BrowserModule,
+    BrowserModule.withServerTransition({appId: 'angular4-lab01-universal'}),
+    FormsModule,
+    HttpModule,
     AppRoutingModule
   ],
   providers: [],
